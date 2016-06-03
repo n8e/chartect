9eb5a67e3195a4bb555d68072c2a7be996a4cb4f
andela-nmartin <godmetweenciati@gmail.com>
2016-06-03 15:13:57 +0300

    adjusts the returned value to an object

--- app/scripts/controllers/dataCtrl.js
+++ app/scripts/controllers/dataCtrl.js
@@ -1,9 +1,22 @@
 angular.module('charts.controllers')
-  .controller('DataCtrl', ['$rootScope', '$scope', '$mdDialog', function($rootScope, $scope, $mdDialog) {
-    $scope.saveUnits = function() {
-      $rootScope.yUnits = $scope.yUnits;
-      $rootScope.xUnits = $scope.xUnits;
-      $rootScope.sample = $scope.sample;
-      $mdDialog.hide();
-    };
-  }]);
+  .controller('DataCtrl', ['$rootScope', '$scope', '$mdDialog',
+    function($rootScope, $scope, $mdDialog) {
+
+      $scope.saveUnits = function() {
+        // create an array of 'sample' empty objects
+        var dataArray = [];
+        for (var i = 0; i < $scope.sample; i++) {
+          dataArray.push({});
+        }
+        $scope.dataArray = dataArray;
+        $rootScope.savedDimensions = {
+          yUnits: $scope.yUnits,
+          xUnits: $scope.xUnits,
+          sample: $scope.sample,
+          dataArray: $scope.dataArray
+        };
+        $mdDialog.hide();
+        return $rootScope.savedDimensions;
+      };
+    }
+  ]);
