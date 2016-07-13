374319d9fb963b4ad264eb833e6f81360b7db31f
Martin Macharia <nate@Martins-MacBook-Pro.local>
2016-07-13 15:16:30 +0300

    removes unnecessary files (we have different schema names, the config file is renamed and we no longer need db-connect)

--- server/config/db-connect.js
+++
@@ -1,8 +0,0 @@
-var Seq = require('sequelize'),
-    sequelize = new Seq('chartsjs', 'postgres', 'root', {
-        host: '127.0.0.1',
-        port: 5433,
-        dialect: 'postgres'
-    });
-
-module.exports = sequelize;
