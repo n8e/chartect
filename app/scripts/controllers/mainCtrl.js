38c2112ba6ebf562983f1e41eb63dd19cb63bf19
Martin Macharia <nate@Martins-MacBook-Pro.local>
2016-07-15 17:21:52 +0300

    refactors the main controller to include only the controller functions for the sidebar

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,8 +1,14 @@
 angular.module('charts.controllers')
-  .controller('MainCtrl', ['$rootScope', '$scope', '$mdDialog', '$mdMedia', '$timeout', '$mdSidenav', '$log',
-    function($rootScope, $scope, $mdDialog, $mdMedia, $timeout, $mdSidenav, $log) {
+  .controller('MainCtrl', ['$rootScope', '$scope', '$mdDialog', '$mdMedia',
+    '$timeout', '$mdSidenav', '$log',
+
+    function($rootScope, $scope, $mdDialog, $mdMedia, $timeout,
+      $mdSidenav, $log) {
+
       $scope.showPrompt = function(ev) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+        var useFullScreen = ($mdMedia('sm') ||
+          $mdMedia('xs')) && $scope.customFullscreen;
+
         $mdDialog.show({
             controller: 'DataCtrl',
             templateUrl: 'views/data.html',
@@ -30,8 +36,8 @@ angular.module('charts.controllers')
       function debounce(func, wait, context) {
         var timer;
         return function debounced() {
-          var context = $scope,
-            args = Array.prototype.slice.call(arguments);
+          context = $scope;
+          var args = Array.prototype.slice.call(arguments);
           $timeout.cancel(timer);
           timer = $timeout(function() {
             timer = undefined;
@@ -45,43 +51,31 @@ angular.module('charts.controllers')
        */
       function buildDelayedToggler(navID) {
         return debounce(function() {
-          // Component lookup should always be available since we are not using `ng-if`
+          // Component lookup should always be available
           $mdSidenav(navID)
             .toggle()
             .then(function() {
-              $log.debug("toggle " + navID + " is done");
+              $log.debug('toggle ' + navID + ' is done');
             });
         }, 200);
       }
 
       function buildToggler(navID) {
         return function() {
-          // Component lookup should always be available since we are not using `ng-if`
+          // Component lookup should always be available
           $mdSidenav(navID)
             .toggle()
             .then(function() {
-              $log.debug("toggle " + navID + " is done");
+              $log.debug('toggle ' + navID + ' is done');
             });
         };
       }
       $scope.close = function() {
-        // Component lookup should always be available since we are not using `ng-if`
+        // Component lookup should always be available -we are not using `ng-if`
         $mdSidenav('left').close()
           .then(function() {
-            $log.debug("close LEFT is done");
+            $log.debug('close LEFT is done');
           });
-      };
-
-      $scope.saveData = function(sample, dataArray) {
-        sample = parseInt(sample);
-        for (var j = 1; j <= sample * 2; j++) {
-          var id = 'input_' + j;
-          if (j % 2 === 0) {
-            dataArray[Math.ceil(j / 2) - 1].y = document.getElementById(id).value;
-          } else {
-            dataArray[Math.ceil(j / 2) - 1].x = document.getElementById(id).value;
-          }
-        }
       };
     }
   ]);
