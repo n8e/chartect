36bcd92c6b0be5151cdbbacfaf5b45c8486fe653
Martin Macharia <nate@Martins-MacBook-Pro.local>
2016-07-15 17:25:10 +0300

    Includes the labelsService function to save the labels for the charts

--- app/scripts/controllers/dataCtrl.js
+++ app/scripts/controllers/dataCtrl.js
@@ -1,6 +1,6 @@
 angular.module('charts.controllers')
-  .controller('DataCtrl', ['$rootScope', '$scope', '$mdDialog',
-    function($rootScope, $scope, $mdDialog) {
+  .controller('DataCtrl', ['$rootScope', '$scope', '$mdDialog', 'labelsService',
+    function($rootScope, $scope, $mdDialog, labelsService) {
 
       $scope.saveUnits = function() {
         // create an array of 'sample' empty objects
@@ -8,13 +8,23 @@ angular.module('charts.controllers')
         for (var i = 0; i < $scope.sample; i++) {
           dataArray.push({});
         }
+
         $scope.dataArray = dataArray;
+
         $rootScope.savedDimensions = {
           yUnits: $scope.yUnits,
           xUnits: $scope.xUnits,
           sample: $scope.sample,
           dataArray: $scope.dataArray
         };
+
+        labelsService.save({
+          y_units: $scope.yUnits,
+          x_units: $scope.xUnits
+        }, function(resp) {
+          $rootScope.labels_id = resp._id;
+        });
+
         $mdDialog.hide();
         return $rootScope.savedDimensions;
       };
